6403eaf2363dea3b07f69d78e2164b7fd5b080e6
Ryotarou Shiozawa <pekoemood@gmail.com>
2026-03-15 00:33:18 +0900

    不要ファイルの削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function() {
+Route::get('/', function() {
     return Inertia::render('Hello');
 });
